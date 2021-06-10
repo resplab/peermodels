c3e3475002b5800e48b9f99f707ae0928ff17401
Amin Adibi <adibi@alumni.ubc.ca>
2021-06-10 11:05:16 -0700

    connect_to_model deprecated and replaced with internal handshake.

--- R/1client_lib.R
+++ R/1client_lib.R
@@ -8,6 +8,58 @@ on_load<-function()
 }
 
 
+
+handshake<-function(model_name, api_key="", local_server = FALSE, bypass_router = FALSE, async = FALSE) #TODO subsequent calls such as get_default_input are problematic with async. Perhaps best to deprecate connect_to_model
+{
+  .Deprecated("model_run")
+  model_name <- str_remove(model_name, "Prism")
+
+  if (!local_server && !bypass_router)  {address <- paste0("https://prism.peermodelsnetwork.com/route/", model_name, "/run")
+  addressObj <- paste0("https://prism.peermodelsnetwork.com/route/", model_name, "/tmp/")}
+
+  if (!local_server && bypass_router)  {address <- paste0("http://model-", model_name, ".cp.prism-ubc.linaralabs.com/ocpu/library/", model_name, "Prism/R/gateway/json")
+  addressObj <- paste0("http://model-", model_name, ".cp.prism-ubc.linaralabs.com/ocpu/tmp/")}
+
+  if (local_server) {address <- paste0("http://localhost:5656/ocpu/library/", model_name,"Prism/R/gateway/json" )
+  addressObj <- paste0("http://localhost:5656/ocpu","/tmp/" )}
+
+  if (!local_server && async && bypass_router)  {address <- paste0("http://model-", model_name, ".cp.prism-ubc.linaralabs.com/ocpu/library/", model_name, "Prism/R/gatewayasync/json")
+  addressObj <- paste0("http://model-", model_name, ".cp.prism-ubc.linaralabs.com/ocpu/tmp/")}
+
+  if (!local_server && async && !bypass_router)  {address <- paste0("https://prism.peermodelsnetwork.com/route/", model_name, "/async/run")
+  addressObj <- paste0("https://prism.peermodelsnetwork.com/route/", model_name, "/tmp/")} #TODO check addressObj for async
+
+  if (local_server && async) {address <- paste0("http://localhost:5656/ocpu/library/", model_name,"Prism/R/gateway_async/json" )
+  addressObj <- paste0("http://localhost:5656/ocpu","/tmp/" )}
+
+
+  on_load()
+  thisSession$api_key<-api_key
+  thisSession$session_id<-NULL
+  thisSession$url <- address
+  thisSession$urlObj <- addressObj
+  thisSession$current_model <- model_name
+
+
+
+  x<-PRISM_call("connect_to_model",api_key=api_key)
+
+  res<-process_input(x)
+
+  if(res$error_code!=0) {message("Error: Could not connect to the model."); return(res$error_code) }
+
+  thisSession$session_id<-res$session_id
+
+  message(res$description)
+
+  return(res)
+
+  #thisSession$default_input<-get_default_input()
+  #thisSession$output_structure<-get_output_structure()
+  #return(0)
+}
+
+
 #' Checks to see if model is available in PRISM
 #'
 #' @param model_name name of the model
@@ -16,6 +68,7 @@ on_load<-function()
 #' @export
 connect_to_model<-function(model_name, api_key="", local_server = FALSE, bypass_router = FALSE, async = FALSE) #TODO subsequent calls such as get_default_input are problematic with async. Perhaps best to deprecate connect_to_model
 {
+  .Deprecated("model_run")
   model_name <- str_remove(model_name, "Prism")
 
   if (!local_server && !bypass_router)  {address <- paste0("https://prism.peermodelsnetwork.com/route/", model_name, "/run")
@@ -68,6 +121,7 @@ connect_to_model<-function(model_name, api_key="", local_server = FALSE, bypass_
 #' @export
 disconnect_from_model<-function()
 {
+  .Deprecated("model_run")
   #fF this is a sessioned connection then the session id will be automatically passed so do not have to submit it!
   x<-PRISM_call("disconnect_from_model")
 
@@ -81,9 +135,14 @@ disconnect_from_model<-function()
 
 #' Returns default PRISM model input
 #'
+#' @param input required custom parameters for current model
+#' @param model_name name of the model
+#' @param local_server whether or not the call should be directed to the server on localhost. Default is FALSE.
+#' @param bypass_router bypass server API router, for debugging purposes
 #' @export
-get_default_input<-function()
+get_default_input<-function(model_name=NULL, local_server=FALSE, bypass_router=FALSE, async=FALSE)
 {
+  handshake(model_name = model_name, local_server = local_server, bypass_router = bypass_router, async = FALSE)
   x<-PRISM_call("get_default_input")
   return(x)
 }
@@ -258,18 +317,24 @@ draw_plots<-function(plot_number=NULL)
 #' Executes PRISM model
 #'
 #' @param input required custom parameters for current model
+#' @param model_name name of the model
+#' @param local_server whether or not the call should be directed to the server on localhost. Default is FALSE.
+#' @param bypass_router bypass server API router, for debugging purposes
+#' @param async should the model be called in async mode?
 #' @return 0 for success and 1 for error
 #' @export
-model_run<-function(input=NULL)
+model_run<-function(input=NULL, model_name=NULL, local_server=FALSE, bypass_router=FALSE, async=FALSE)
 {
+  handshake(model_name = model_name, local_server = local_server, bypass_router = bypass_router, async = async)
+
   thisSession$input<-input
 
-    res<-PRISM_call("prism_model_run", model_input=input)
+  res<-PRISM_call("prism_model_run", model_input=input)
 
-    thisSession$output_location<-thisSession$last_location
-    thisSession$output_list<-NULL
+  thisSession$output_location<-thisSession$last_location
+  thisSession$output_list<-NULL
 
-    thisSession$result<-res
+  thisSession$result<-res
 
     #thisSession$model_output_objects<-PRISM_get_output_object_list()
 
