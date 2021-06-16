d4f4fbd6078d5ecbdd707e56bcdf1a0f01a39ce7
Amin Adibi <adibi@alumni.ubc.ca>
2021-06-16 15:12:40 -0700

    handshake deprecated

--- R/1client_lib.R
+++ R/1client_lib.R
@@ -316,7 +316,34 @@ draw_plots<-function(plot_number=NULL)
 #' @export
 model_run<-function(input=NULL, model_name=NULL, api_key = "", local_server=FALSE, bypass_router=FALSE, async=FALSE)
 {
-  handshake(model_name = model_name, api_key = api_key, local_server = local_server, bypass_router = bypass_router, async = async)
+  #handshake(model_name = model_name, api_key = api_key, local_server = local_server, bypass_router = bypass_router, async = async)
+  model_name <- str_remove(model_name, "Prism")
+
+  if (!local_server && !bypass_router)  {address <- paste0("https://prism.peermodelsnetwork.com/route/", model_name, "/run")
+  addressObj <- paste0("https://prism.peermodelsnetwork.com/route/", model_name, "/tmp/")}
+
+  if (!local_server && bypass_router)  {address <- paste0("http://model-", model_name, ".cp.prism-ubc.linaralabs.com/ocpu/library/", model_name, "Prism/R/gateway/json")
+  addressObj <- paste0("http://model-", model_name, ".cp.prism-ubc.linaralabs.com/ocpu/tmp/")}
+
+  if (local_server) {address <- paste0("http://localhost:5656/ocpu/library/", model_name,"Prism/R/gateway/json" )
+  addressObj <- paste0("http://localhost:5656/ocpu","/tmp/" )}
+
+  if (!local_server && async && bypass_router)  {address <- paste0("http://model-", model_name, ".cp.prism-ubc.linaralabs.com/ocpu/library/", model_name, "Prism/R/gatewayasync/json")
+  addressObj <- paste0("http://model-", model_name, ".cp.prism-ubc.linaralabs.com/ocpu/tmp/")}
+
+  if (!local_server && async && !bypass_router)  {address <- paste0("https://prism.peermodelsnetwork.com/route/", model_name, "/async/run")
+  addressObj <- paste0("https://prism.peermodelsnetwork.com/route/", model_name, "/tmp/")} #TODO check addressObj for async
+
+  if (local_server && async) {address <- paste0("http://localhost:5656/ocpu/library/", model_name,"Prism/R/gatewayasync/json" )
+  addressObj <- paste0("http://localhost:5656/ocpu","/tmp/" )}
+
+
+  on_load()
+  thisSession$api_key<-api_key
+  thisSession$session_id<-NULL
+  thisSession$url <- address
+  thisSession$urlObj <- addressObj
+  thisSession$current_model <- model_name
 
   thisSession$input<-input
 
